172cf13733846c847ec0d6e127fdae90b98f9b7e
yuorky <34466028+yuorky@users.noreply.github.com>
2019-11-29 16:48:17 -0500

    Add files via upload

--- data/Dataset.py
+++ data/Dataset.py
@@ -6,11 +6,12 @@ from torchvision import transforms as T
 
 class Proteins(data.Dataset):
     'Characterize a dataset for Pytorch'
-    def __init__(self, root):
+    def __init__(self, root, transforms=None):
         'Initialization'
         imgs = os.listdir(root)
         #这里不实际加载图片，只是指定路径
-        self.imgs = [os.path.join(root,img) for img in imgs]
+        self.imgs = [os.path.join(root, img) for img in imgs]
+        self.transforms = transforms
 
     def __len__(self):
         'Denotes the total number of samples'
@@ -18,11 +19,11 @@ class Proteins(data.Dataset):
 
     def __getitem__(self, index):
         'Generate one sample of data'
-        # select sample
-        ID = self.list_IDs[index]
-        # Load data and get label
-        X = torch.load('data/' + ID + '.pt')
-        y = self.labels[ID]
-
-        return X,y
+        img_path = self.imgs[index]
+        # AB-40 -> "AB-40"; Alpha-syn -> "Alpha-syn"
+        label = "AB-40" if "AB-40" in img_path.split('/')[-1] else "Alpha-syn"
+        data = Image.open(img_path)
+        if self.transforms:
+            data = self.transforms(data)
+        return data, label
 
